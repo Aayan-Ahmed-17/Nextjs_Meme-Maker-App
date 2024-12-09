470e51e5a860b565d52dcf903e05bf1575ae6689
Aayan Ahmed <aayanahmedtejani@gmail.com>
2024-12-09 19:42:39 +0500

    bug solved

--- app/creatememe/page.jsx
+++ app/creatememe/page.jsx
@@ -8,19 +8,22 @@ const Creatememe = ({ searchParams }) => {
   const text1 = useRef();
   const text2 = useRef();
 
-  const generateMemes = async (e) => {
-    e.preventDefault();
+  const handleCreateMeme = async (event) => {
+    event.preventDefault();
+    console.log(text1?.current?.value);
+    console.log(text2?.current?.value);
 
     const data = await fetch(
-      `https://api.imgflip.com/caption_image?template_id=${id}&username=MuhammadHaris2&password=qureshi12&text0=${text1.current?.value}&text1=${text2.current?.value}`,
+      `https://api.imgflip.com/caption_image?template_id=${id}&username=Aayan_Ahmed&password=ragosir383@lofiey.com123&text0=${text1.current?.value}&text1=${text2.current?.value}`,
       {
         method: "POST",
       }
     );
     const response = await data.json();
-    console.log(response.data.url);
-    setImg(response.data.url);
+    console.log(response);
+    setImg(response?.data?.url);
   };
+ 
 
   return (
     <>
@@ -41,7 +44,7 @@ const Creatememe = ({ searchParams }) => {
           </div>
 
           <form
-            onSubmit={generateMemes}
+            onSubmit={handleCreateMeme}
             className="col-span-2 flex flex-wrap gap-1 items-center h-1/3 place-self-center"
           >
             <input
@@ -68,18 +71,18 @@ const Creatememe = ({ searchParams }) => {
       </div>
 
       <dialog id="my_modal_1" className="modal overflow-hidden">
-        <div className="modal-box pb-6 w-[25em]">
+        <div className="modal-box pb-6">
           {img ? (
-            <img src={img} alt="final image" className="h-[25rem]"/>
+            <img src={img} alt="final image" />
           ) : (
             <div className=" text-center">
               <span className="loading loading-spinner loading-lg"></span>
             </div>
           )}{" "}
           <div className="modal-action">
-            <form method="dialog " className="-mt-3">
+            <form method="dialog"  className="-mt-3">
               {/* if there is a button in form, it will close the modal */}
-              <button className="btn">Close</button>
+              <button className="btn" type="submit">Close</button>
             </form>
           </div>
         </div>
@@ -87,86 +90,5 @@ const Creatememe = ({ searchParams }) => {
     </>
   );
 };
-
-// export default Creatememe;
-
-// 'use client'
-// import React, { useRef, useState } from 'react'
-// import { useSearchParams } from 'next/navigation';
-
-// const Creatememe = ({ searchParams }) => {
-//     //   const [img, setImg] = useState("");
-//       const { url , id} = React.use(searchParams);
-//       const text1 = useRef();
-//       const text2 = useRef();
-
-//     // const searchParams = useSearchParams(); // Hook to access search parameters
-//     // const url = searchParams.get('url'); // Replace 'url' with your query parameter key
-//     // const id = searchParams.get('id'); // Replace 'url' with your query parameter key
-
-//     React.useEffect(() => {
-//         if (url && id) {
-//           console.log(`URL Param: ${url}`);
-
-//           console.log(`URL Param: ${id}`);
-//           // Add your logic here
-//         }
-//       }, [url]);
-// const [img,setImg]=  useState('')
-
-// const generateMemes= async (e)=>{
-//  e.preventDefault()
-//     console.log(text1.current.value,text2.current.value)
-
-// const data = await fetch(`https://api.imgflip.com/caption_image?template_id=${id}&username=MuhammadHaris2&password=qureshi12&text0=${text1.current?.value}&text1=${text2.current?.value}` ,
-//     {
-//     method: 'POST'
-// })
-// const response = await data.json()
-// console.log(response.data.url);
-// setImg(response.data.url)
-
-// }
-
-//     return (
-//         <div className=''>
-//             <br />
-//             <div className="card lg:card-side bg-base-100  my-5 m-auto  w-[60%] shadow-xl">
-//                 <figure style={{ width: "40%" , }}>
-//                     <img src={url}
-//                         alt="Album" />
-//                 </figure>
-//                 <div className="card-body">
-//                     <form action="" onSubmit={generateMemes} className='m-auto text-center '>
-//                         <input type="text" placeholder="Enter your Memes Text? " ref={text1} className="input input-bordered m-2 w-full max-w-xs" />
-//                         <input type="text" placeholder="Enter your Memes Text? " ref={text2} className="input input-bordered m-2 w-full max-w-xs" />
-//                         <br />
-//                         <button className="btn btn-neutral m-2" onClick={()=>document.getElementById('my_modal_1').showModal()}>Generate</button>
-
-//                     </form>
-//                 </div>
-//             </div>
-
-//             <p></p>
-
-// <dialog id="my_modal_1" className="modal">
-//   <div className="modal-box">
-//  { img ? <img src={img}
-//    alt="meme generated image" />  : <div className=' text-center'>
-// <span className="loading loading-spinner loading-lg"></span>
-//    </div>
-
-//  }   <div className="modal-action">
-//       <form method="dialog">
-//         {/* if there is a button in form, it will close the modal */}
-//         <button className="btn">Close</button>
-//       </form>
-//     </div>
-//   </div>
-// </dialog>
-//         </div>
-//     )
-
-// }
 
 export default Creatememe;
